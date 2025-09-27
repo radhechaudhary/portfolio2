a1a3c194f9b75df7252950c84a1aa854ff385e91
Radhe Chaudhary <radhechaudhary6398@gmail.com>
2025-09-27 17:42:49 +0530

    spotlight

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,43 @@ import About from './components/About'
 import HowICanHelp from './components/help_you'
 import Footer from './components/footer'
 import Contact from './components/contactButton'
+import { useEffect, useState } from 'react'
+import {motion} from 'framer-motion'
+
 
 function App(){
+
+  const [position, setPosition] = useState({x:0, y:0});
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+
   return (
    <div className=' text-white overflow-x-hidden scroll-smooth'>
+    <motion.div
+        className="w-1 h-1 rounded-full bg-[#99a1af] opacity-50 pointer-events-none fixed z-100"
+        animate={{
+        x: position.x - 0.5, // center the circle
+        y: position.y - 0.5,
+      }}
+      transition={{
+        type: "spring",
+        stiffness: 500,
+        damping: 40,
+      }}
+      style={{
+        boxShadow: "0 0 70px 70px #99a1af",
+      }}
+      ></motion.div>
       <div className='bg-[#2b2b2b] pt-[50vh] px-5'>
           <Contact/>
           <Landing/>
