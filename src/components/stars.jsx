26530ee0d8280121021321e151f1391f2187c0f5
Radhe Chaudhary <radhechaudhary6398@gmail.com>
2025-10-14 22:44:57 +0530

    stars movement

--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -1,11 +1,28 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+
+
+
 
 function MovingStars(props) {
+  const [position, setPosition] = useState({x:0, y:0});
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   const ref = useRef();
   useFrame(() => {
-    ref.current.rotation.y += 0.001; // slow rotation
+    ref.current.rotation.y = -0.002 * position.x; // slow rotation
+    ref.current.rotation.x = -0.002 * position.y;
+    
   });
   return <Stars ref={ref} {...props} />;
 }
